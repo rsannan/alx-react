d06c06d6a858ec560e9fca32938d5416733f5c41
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-20 19:40:25 +0000

    add task 3

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,24 @@
+import $  from "jquery";
+import _  from "lodash";
+import '../css/main.css';
+import image from "../assets/holberton-logo.jpg";
+
+const myImage = new Image();
+myImage.scr = image;
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+$('body').prepend("<div id='logo'></div>");
+
+
+let count = 0;
+function updateCounter() {
+    count += 1;
+    $("#count").empty();
+    $("#count").append( `${count} clicks on the button` );
+  };
+
+  $("button").on("click", _.debounce(updateCounter, 500));
+
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  performance: {
+    maxAssetSize: 1048576,
+  },
+};
