e6efb4ff6956248f4dfe0b2be619b166a51ae13a
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-20 17:07:12 +0000

    fix task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,13 +1,14 @@
 import $  from "jquery";
 import _  from "lodash";
 
+let count = 0;
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
 function updateCounter() {
     count += 1;
     $("#count").empty();
