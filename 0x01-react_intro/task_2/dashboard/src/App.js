f65f3625944d1c7e44a3064078938cea8b1377d5
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-22 00:42:45 +0000

    unfinished task 2

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -12,6 +12,15 @@ function App() {
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <div className="form">
+          <label>Email: </label>
+          <input type="email" name="email" id="email" />
+
+          <label>Password: </label>
+          <input type="password" name="password" id="password" />
+
+          <button>OK</button>
+        </div>
       </div>
       <div className="App-footer">
         <p>
--- 0x01-react_intro/task_2/dashboard/src/Notifications..js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications..js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Notifications.css";
+import closeIcon from "./icons8-close-window-24.png";
 
+const notificationStyle = {
+  position: "absolute",
+  right: 0,
+  top: 0,
+  padding: 0,
+  border: 0,
+};
+
+function btnClick() {
+  return console.log("Close button has been clicked");
+}
 export default function showNotification() {
   return (
     <div className="root-notifications">
+      <button style={notificationStyle} aria-label="Close" onClick={btnClick}>
+        <img src={closeIcon} alt="close icon" />
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li>New course available</li>
+        <li>New resume available</li>
+      </ul>
     </div>
   );
 }
