a3ac259d44c283be4df12fb9d04d5520ad631a47
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-22 14:51:56 +0000

    add tests

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { render, screen, cleanup } from "@testing-library/react";
 
-test("test", () => {
-  expect(true).toBe(true);
+test("should render App", () => {
+  const { testapp } = render(<App />);
+  expect(testapp).toBeIntheDocument();
 });
