42dc0861ecbb2747a70abccc918250649c486fc2
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-22 12:36:11 +0000

    finish task 2

--- 0x01-react_intro/task_2/dashboard/src/Notifications..js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications..js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Notifications.css";
 import closeIcon from "./icons8-close-window-24.png";
+import { getLatestNotification } from "./utils";
 
 const notificationStyle = {
   position: "absolute",
@@ -21,8 +22,9 @@ export default function showNotification() {
       </button>
       <p>Here is the list of notifications</p>
       <ul>
-        <li>New course available</li>
-        <li>New resume available</li>
+        <li data="default">New course available</li>
+        <li data="urgent">New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
       </ul>
     </div>
   );
