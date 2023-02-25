03e2bb2419bd122b1ff4cc0796932bef6030f6f4
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-25 16:40:01 +0000

    add header folder

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,14 +1,10 @@
-import './App.css';
-import logo from '../assets/holberton_logo.jpg';
-import { getFooterCopy, getFullYear } from '../utils/utils';
+import "./App.css";
+import logo from "../assets/holberton_logo.jpg";
+import { getFooterCopy, getFullYear } from "../utils/utils";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
         <label htmlFor="email">Email: </label>
@@ -18,7 +14,9 @@ function App() {
         <button>OK</button>
       </div>
       <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
       </div>
     </div>
   );
---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+<div className="App-header">
+  <img src={logo} alt="logo" />
+  <h1>School dashboard</h1>
+</div>;
