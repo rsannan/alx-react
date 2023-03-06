cf106f7ad3103ebb565d9ea40378b8ba2cbef7e1
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-03-06 15:40:57 +0000

    add style

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const styleRow = {
+    backgroundColor: "#f5f5f5ab",
+  };
+  const styleHeaderRow = {
+    backgroundColor: "#deb5b545",
+  };
+
+  const trueStyle = isHeader ? styleHeaderRow : styleRow;
   return (
-    <tr>
-      {isHeader ?
-        textSecondCell === null ?
+    <tr style={trueStyle}>
+      {isHeader ? (
+        textSecondCell === null ? (
           <th colSpan="2">{textFirstCell}</th>
-        :
+        ) : (
           <>
             <th>{textFirstCell}</th>
             <th>{textSecondCell}</th>
           </>
-      :
+        )
+      ) : (
         <>
           <td>{textFirstCell}</td>
           <td>{textSecondCell}</td>
         </>
-      }
+      )}
     </tr>
   );
 }
 
 CourseListRow.defaultProps = {
   isHeader: false,
-  textSecondCell: null
+  textSecondCell: null,
 };
 
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
   textFirstCell: PropTypes.string,
-  textSecondCell: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ])
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 export default CourseListRow;
