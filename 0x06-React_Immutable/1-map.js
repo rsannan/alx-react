189d7539b9b41b641276190178c9550f1ddef4e6
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-03-15 18:29:09 +0000

    fix task 1

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,5 @@
-import Immutable from 'immutable';
+import { Immutable } from 'immutable';
 
-export default function getImmutableObject (object) {
+export default function getImmutableObject(object) {
   return Immutable.Map(object);
 }
