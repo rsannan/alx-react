81aabae8afc5fd395a1378e6e5685e1c12c33476
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-03-15 03:58:20 +0000

    fix task 0

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,9 +1,7 @@
 const Immutable = require("immutable");
 
-export function getImmutableObject(object) {
-  const transform = Immutable.fromJS(object, (key, value) => {
-    return value.toMap();
-  });
-
-  return transform;
+function getImmutableObject(object) {
+  return Immutable.fromJS(object);
 }
+
+module.exports = { getImmutableObject };
