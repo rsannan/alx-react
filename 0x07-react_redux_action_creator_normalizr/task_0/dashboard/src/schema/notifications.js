d23c3a2d4ac5355d169e2128e69def3251810b27
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-03-21 00:22:10 +0000

    add notification.js schema

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -3,11 +3,11 @@ import * as Notis from "../../notifications.json" assert { type: "json" };
 export default function getAllNotificationsByUser(userid) {
   let contextarr = [];
   for (let obj of Notis.default) {
-    if (userid === obj.id) {
-      console.log("test");
-      console.log(obj.context);
+    if (userid === obj.author.id) {
+      contextarr.push(obj.context);
     }
   }
+  return contextarr;
 }
-
-getAllNotificationsByUser("5debd764a7c57c7839d722e9");
+const id = "5debd764a7c57c7839d722e9";
+console.log(getAllNotificationsByUser(id));
