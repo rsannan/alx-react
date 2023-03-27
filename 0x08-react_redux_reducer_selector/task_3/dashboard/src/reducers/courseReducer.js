1c2f203f56c310b9c65170f802b9ff34dd55a7af
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-03-27 15:33:34 +0100

    add task 2

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.js
@@ -0,0 +1,31 @@
+import {
+  SELECT_COURSE,
+  FETCH_COURSE_SUCCESS,
+  UNSELECT_COURSE,
+} from "../actions/courseActionTypes";
+const defaultState = [];
+
+export default function courseReducer(state = defaultState, action) {
+  switch (action.type) {
+    case FETCH_COURSE_SUCCESS:
+      return action.data.map((course) => {
+        return { ...course, isSelected: false };
+      });
+
+    case SELECT_COURSE:
+      return state
+        .filter((course) => course.id === action.data.index)
+        .map((course) => {
+          return { ...course, isSelected: true };
+        });
+
+    case UNSELECT_COURSE:
+      return state
+        .filter((course) => course.id === action.data.index)
+        .map((course) => {
+          return { ...course, isSelected: false };
+        });
+    default:
+      return state;
+  }
+}
